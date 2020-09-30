bc2f0e0bfc599775b6a7cc61103b391ccda50447
LeeYumHarbec <liam.harbec13@gmail.com>
2020-09-30 13:52:24 -0400

    Initialized basic project, plugin loads on startup

---
+++ src/com/aearost/irohstea/Main.java
@@ -0,0 +1,12 @@
+package com.aearost.irohstea;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// Add initializations
+	}
+}
