6eff261955eefb8761bcd53e6c2fc56d0a4c603d
LeeYumHarbec <liam.harbec13@gmail.com>
2020-10-04 15:11:11 -0400

    Fixed all messages. Proper messages are delivered!

--- src/com/aearost/commands/CommandTeas.java
+++ src/com/aearost/commands/CommandTeas.java
@@ -1,9 +1,6 @@
 package com.aearost.commands;
 
-import java.util.ListIterator;
-
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,62 +19,97 @@ public class CommandTeas implements CommandExecutor {
 			return false;
 		}
 
-		// /teas give Aearost TEA_LEAF 7
-
 		if (args.length >= 3 && args[0].equals("give")) {
 			Player target = Bukkit.getPlayer(args[1]);
-			
-			if (args.length == 3) {
-				if (args[2].equals("TEA_LEAF")) {
-					return giveItem(TeaLeaf.getTeaLeaf(), target, sender);
+			if (args[2].equals("TEA_LEAF")) {
+				if (args.length == 4) {
+					int amount;
+					try {
+						amount = Integer.parseInt(args[3]);
+					} catch (NumberFormatException e) {
+						sender.sendMessage(Utils.chatMessage("&eYou must enter a valid quantity!"));
+						return false;
+					}
+					// In valid inventory slot range
+					if (amount > 0 && amount <= Utils.MAXIMUM_ITEM_AMOUNT) {
+						ItemStack teaLeaf = TeaLeaf.getTeaLeaf();
+						teaLeaf.setAmount(amount);
+						return giveItem(teaLeaf, target, sender);
+					} else {
+						sender.sendMessage(Utils.chatMessage("&cThat is not a valid amount!"));
+					}
+				} else {
+					ItemStack teaLeaf = TeaLeaf.getTeaLeaf();
+					// If not specified, will give 64
+					teaLeaf.setAmount(64);
+					return giveItem(teaLeaf, target, sender);
 				}
 			}
-			// Logic here for specified amount
-			// else if (args.length == 4) {
 		}
 		return false;
 	}
 
 	private boolean giveItem(ItemStack itemToAdd, Player target, CommandSender sender) {
 		if (target != null) {
-			if (Utils.hasInventorySpace(target)) {
-				target.getInventory().addItem(itemToAdd);
-				return (isSenderAnotherPlayer(itemToAdd, target, sender));
+			ItemStack copyForHasSpace = itemToAdd.clone();
+			int remainder = Utils.hasInventorySpace(target, copyForHasSpace);
+			if (remainder == 0) {
+				return sendMessages(itemToAdd, target, sender, 0);
+			} else if (remainder == -1) {
+				return sendMessages(itemToAdd, target, sender, -1);
+			} else {
+				return sendMessages(itemToAdd, target, sender, remainder);
 			}
-			
-			ListIterator<ItemStack> iterator = target.getInventory().iterator();
-			// Checks if the ItemStack already is in the inventory
-			while (iterator.hasNext()) {
-				int iteratedIndex = iterator.nextIndex();
-				ItemStack is = iterator.next();
-				
-				// If it's the same item
-				if (is != null && is.getType() == itemToAdd.getType() && Utils.isMatchingItemStack(is, itemToAdd)) {
-					// If the ItemStack isn't full
-					if (is.getAmount() < is.getMaxStackSize()) {
-						is.setAmount(is.getAmount() + 1);
-						target.getInventory().setItem(iteratedIndex, is);
-						return isSenderAnotherPlayer(itemToAdd, target, sender);
-					}
-				}
-			}
-
 		} else {
 			sender.sendMessage(Utils.chatMessage("&cThat player is not online!"));
 		}
 		return false;
 	}
-	
-	private boolean isSenderAnotherPlayer(ItemStack itemToAdd, Player target, CommandSender sender) {
+
+	private boolean sendMessages(ItemStack itemToGive, Player target, CommandSender sender, int remainingAmount) {
+		// If the the sender gave themselves the item
 		if (sender instanceof Player) {
-			Player commandSender = (Player) sender;
-			target.sendMessage(Utils.chatMessage("&cYou have been given 1 &e" + itemToAdd.getItemMeta().getDisplayName() + "&c!"));
-			if (!commandSender.equals(target)) {
-				commandSender.sendMessage(Utils.chatMessage("&e" + target.getDisplayName() +
-						" &chas been given 1 " + itemToAdd.getItemMeta().getDisplayName()));
+			Player senderAsPlayer = (Player) sender;
+			String itemName = itemToGive.getItemMeta().getDisplayName();
+			
+			// If the sender is also the target
+			if (senderAsPlayer.getName().equals(target.getName())) {
+				if (remainingAmount == 0) {
+					target.sendMessage(Utils
+							.chatMessage("&6You have been given &a" + itemToGive.getAmount() + " " + itemName + "&6!"));
+					return true;
+				} else if (remainingAmount > 0) {
+					int amountGiven = itemToGive.getAmount() - remainingAmount;
+					target.sendMessage(
+							Utils.chatMessage("&6You have been given &a" + amountGiven + " " + itemName + "&6!"));
+					target.sendMessage(
+							Utils.chatMessage("&a" + remainingAmount + " " + itemName + " &6was thrown away!"));
+					return true;
+				} else {
+					target.sendMessage(Utils.chatMessage("&cYou do not have enough space for that!"));
+					return false;
+				}
 			}
 		}
-		return true;
-	}
 
+		// If someone else (including console) gave them the item
+		String itemName = itemToGive.getItemMeta().getDisplayName();
+		if (remainingAmount == 0) {
+			target.sendMessage(
+					Utils.chatMessage("&6You have been given &a" + itemToGive.getAmount() + " " + itemName + "&6!"));
+			sender.sendMessage(Utils.chatMessage("&e" + target.getName() + " &6has been given &a"
+					+ itemToGive.getAmount() + " " + itemName + "&6!"));
+			return true;
+		} else if (remainingAmount > 0) {
+			int amountGiven = itemToGive.getAmount() - remainingAmount;
+			target.sendMessage(Utils.chatMessage("&6You have been given &a" + amountGiven + " " + itemName + "&6!"));
+			sender.sendMessage(Utils.chatMessage(
+					"&e" + target.getName() + " &6has been given &a" + amountGiven + " " + itemName + "&6!"));
+			sender.sendMessage(Utils.chatMessage("&a" + remainingAmount + " " + itemName + " &6was thrown away!"));
+			return true;
+		} else {
+			sender.sendMessage(Utils.chatMessage("&7" + target.getName() + " &cdoes not have enough space for that!"));
+			return false;
+		}
+	}
 }
--- src/com/aearost/irohstea/Utils.java
+++ src/com/aearost/irohstea/Utils.java
@@ -1,12 +1,12 @@
 package com.aearost.irohstea;
 
-import java.util.ListIterator;
-
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class Utils {
+	
+	public final static int MAXIMUM_ITEM_AMOUNT = 2034;
 	
 	/**
 	 * Allows the formatting of messages to contain Minecraft colors
@@ -28,9 +28,75 @@ public class Utils {
 		return ChatColor.translateAlternateColorCodes('&', msg);
 	}
 	
-	public static boolean hasInventorySpace(Player player)
+	public static int hasInventorySpace(Player player, ItemStack itemToAdd)
 	{
-	    return player.getInventory().firstEmpty() != -1;
+		int amount = itemToAdd.getAmount();
+//		player.sendMessage("Amount at beginning: " + amount);
+		
+		// Prioritizes filling up non-full stacks of the item in the player's inventory
+	    ItemStack[] inventory = player.getInventory().getStorageContents();
+	    for (ItemStack is : inventory) {
+	    	if (is != null) {
+	    		if (amount == 0) {
+//	    			player.sendMessage("Amount is 0");
+	    			return 0;
+	    		}
+	    		// Fill up an empty stack until it's full while removing one amount each iteration
+	    		if (is.getItemMeta().getDisplayName().equals(itemToAdd.getItemMeta().getDisplayName())) {
+//	    			player.sendMessage("Item is tea leaf");
+	    			while (amount > 0) {
+//	    				player.sendMessage("------------");
+//	    				player.sendMessage("Amount is: " + amount);
+//	    				player.sendMessage("is.getAmount() is: " + is.getAmount());
+	    				if (is.getAmount() < 64) {
+//	    					player.sendMessage("is amount before addition: " + is.getAmount());
+	    					is.setAmount(is.getAmount() + 1);
+	    					amount--;
+	    				} else {
+//	    					player.sendMessage("Breaking");
+	    					break;
+	    				}
+	    			}
+	    		}
+	    	}
+	    }
+	    
+	    player.sendMessage("*******************");
+	    player.sendMessage("Amount after first half is: " + amount);
+	    player.sendMessage("*******************");
+	    
+	    // Prioritizes filling up empty inventory slots
+	    // Infinite loop somewhere in here, figure it out!
+	    ItemStack is = itemToAdd.clone();
+	    while (amount > 0) {
+	    	// When there is inventory space
+	    	if (player.getInventory().firstEmpty() != -1) {
+	    		player.sendMessage("Empty inventory slots");
+	    		if (amount > 64) {
+	    			player.sendMessage("amount more than 64, currently is: " + amount);
+	    			is = itemToAdd.clone();
+	    			is.setAmount(64);
+	    			player.getInventory().addItem(is);
+	    			amount = amount - 64;
+	    			player.sendMessage("amount is now: " + amount);
+	    		} else {
+	    			player.sendMessage("amount is less than 64, adding final amount: " + amount);
+	    			is = itemToAdd.clone();
+	    			is.setAmount(amount);
+	    			player.getInventory().addItem(is);
+	    			return 0;
+	    		}
+	    	} else if (amount < itemToAdd.getAmount()) {
+	    		player.sendMessage("No empty player slots and " + amount + " < " + itemToAdd.getAmount());
+	    		return amount;
+    		// No space in the inventory
+	    	} else {
+	    		player.sendMessage("No space for anything at all");
+	    		return -1;
+	    	}
+	    }
+	    player.sendMessage("OOP");
+	    return -1;
 	}
 	
 	public static boolean isMatchingItemStack(ItemStack inventoryItem, ItemStack itemToAdd) {
